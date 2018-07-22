c25fd2c4451464069bee60c8384986221865de1d
Raymond Fallon <Raymond.Fallon@pennmedicine.upenn.edu>
2018-07-22 11:16:44 -0400

    Make a Welcome Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope ENV['RAILS_RELATIVE_URL_ROOT'] do
+    root to: 'welcome#show'
+  end
 end
