3ad3c64625b22b611b9304a369bf52af1ae67ffc
Raymond Fallon <Raymond.Fallon@pennmedicine.upenn.edu>
2018-09-11 18:38:20 -0400

    Create Static Pages Controller, Donations Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#show'
 
+  get :donations, to: 'static_pages#donations', as: :donations
+
   resources :members do
     resources :castings
   end
