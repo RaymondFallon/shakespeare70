fb7d20bfc6cd34ecb05b021e7e64735c2fbf5e54
Raymond Fallon <Raymond.Fallon@pennmedicine.upenn.edu>
2018-09-16 13:39:30 -0400

    Add Mike And Curt Seed Files

--- db/seeds/members/curt_foxworth.rb
+++ db/seeds/members/curt_foxworth.rb
@@ -6,5 +6,44 @@ curt = Member.where(first_name: 'Curt', last_name: 'Foxworth')
              .first_or_create!(exec_committee: 'Y',
                                featured: 'Y',
                                title: 'Director • Actor • Production Staff',
-                               bio: "Curt Foxworth is a 2002 graduate of The College of New Jersey.  He studied improvisation at IO Theater in Chicago.  He is a member of SAG-AFTRA and currently teaches theater at Monmouth University.  Curt made his first appearance with Shakespeare ‘70 in its 2002 production of \"A Midsummer Night’s Dream\" at Washington Crossing State Park.")
+                               bio: 'Curt Foxworth is a 2002 graduate of The College of New Jersey.  He studied improvisation at IO Theater in Chicago.  He is a member of SAG-AFTRA and currently teaches theater at Monmouth University.  Curt made his first appearance with Shakespeare ‘70 in its 2002 production of <em>A Midsummer Night’s Dream</em> at Washington Crossing State Park.')
+
 s70 = Company.where(name: "Shakespeare '70").first_or_create!
+act = Company.where(name: "TCNJ's All College Theater").first_or_create!
+
+s70_bt = BioType.where(code: 'S70', description: 'Shakespeare 70').first_or_create!
+oc_bt =  BioType.where(code: 'OC', description: 'Other Company').first_or_create!
+
+actor =    Position.where(code: 'A', description: 'Actor').first_or_create!
+director = Position.where(code: 'D', description: 'Director').first_or_create!
+co_dir =   Position.where(code: 'CO', description: 'Co-Director').first_or_create!
+a_dir =    Position.where(code: 'AD', description: 'Assistant Director').first_or_create!
+pm =       Position.where(code: 'PM', description: 'Production Manager').first_or_create!
+cpm =      Position.where(code: 'CPM', description: 'Co-Production Manager').first_or_create!
+
+[
+    [s70, s70_bt, co_dir, 'Murder Most Foul: An Evening of Shakespeare', 2017],
+    [s70, s70_bt, pm, 'Measure For Measure', 2017],
+    [s70, s70_bt, actor, 'The Heidi Chronicles', 2017, 'Peter'],
+    [s70, s70_bt, pm, 'Hamlet', 2016],
+    [s70, s70_bt, cpm, 'Hedda Gabler', 2016],
+    [s70, s70_bt, actor, 'The Winter’s Tale', 2015, 'Camillo'],
+    [s70, s70_bt, director, 'Becky Shaw', 2015],
+    [s70, s70_bt, actor, 'Much Ado About Nothing', 2014, 'Don John'],
+    [s70, s70_bt, director, 'The Vortex', 2013],
+    [s70, s70_bt, actor, 'As You Like It', 2013, 'Jaques'],
+    [s70, s70_bt, a_dir, 'Antigone', 2011],
+    [s70, s70_bt, actor, 'Hay Fever', 2005, 'Simon'],
+    [s70, s70_bt, actor, 'The Way of the World', 2004, 'Petulant'],
+    [s70, s70_bt, actor, 'Twelfth Night', 2004, 'Feste'],
+    [s70, s70_bt, actor, "'A Midsummer Night's Dream'", 2002, 'Flute'],
+
+    [act, oc_bt, director, 'Romeo & Juliet', 2014],
+    [act, oc_bt, director, 'As You Like It', 2016],
+    [act, oc_bt, director, 'The Secret in the Wings', 2017]
+].each do |casting|
+  Casting.where(member: curt,
+                production: Production.where(company: casting[0], title: casting[3], year: casting[4]).first_or_create!,
+                position: casting[2],
+                bio_type: casting[1]).first_or_create!(role: casting[5])
+end
--- db/seeds/members/michael_krahel.rb
+++ db/seeds/members/michael_krahel.rb
@@ -1,10 +1,68 @@
 # frozen_string_literal: true
 
-# Initial Bio For Janet Quartarone
+# Initial Bio For Michael Krahel
 
-mike = Member.where(first_name: 'Janet', last_name: 'Quartarone')
+mike = Member.where(first_name: 'Michael', last_name: 'Krahel')
              .first_or_create!(exec_committee: 'Y',
                                featured: 'Y',
                                title: 'Actor • Production Staff • Director',
-                               bio: "Michael Krahel graduated from The College of New Jersey in 2009. He currently works as an administrative analyst for the New Jersey Department of Environmental Protection.  Mike made his first appearance with Shakespeare ‘70 in its 2008 production of \"The Skin of Our Teeth\" at TCNJ’s Kendall Main Stage.")
+                               bio: "Michael Krahel graduated from The College of New Jersey in 2009. He currently works as an administrative analyst for the New Jersey Department of Environmental Protection.  Mike made his first appearance with Shakespeare ‘70 in its 2008 production of <em>The Skin of Our Teeth</em> at TCNJ’s Kendall Main Stage.")
 s70 = Company.where(name: "Shakespeare '70").first_or_create!
+
+this_bt = BioType.where(code: 'TS', description: 'This Season').first_or_create!
+s70_bt =  BioType.where(code: 'S70', description: 'Shakespeare 70').first_or_create!
+oc_bt =   BioType.where(code: 'OC', description: 'Other Company').first_or_create!
+
+actor =    Position.where(code: 'A', description: 'Actor').first_or_create!
+director = Position.where(code: 'D', description: 'Director').first_or_create!
+lights =   Position.where(code: 'LD', description: 'Lighting Designer').first_or_create!
+sm =       Position.where(code: 'SM', description: 'Stage Manager').first_or_create!
+
+[
+    [s70, this_bt, actor, 'The Merry Wives of Windsor', 'Frank Ford'],
+
+    [s70, s70_bt, actor, 'Murder Most Foul: An Evening of Shakespeare', 'Ensemble'],
+    [s70, s70_bt, lights, 'Murder Most Foul: An Evening of Shakespeare'],
+    [s70, s70_bt, actor, 'Measure for Measure', 'Claudio'],
+    [s70, s70_bt, actor, "The Doctor's Dilemma", 'Louis Dubedat'],
+    [s70, s70_bt, actor, 'Hamlet', 'Rosencrantz'],
+    [s70, s70_bt, director, 'Hedda Gabler'],
+    [s70, s70_bt, actor, 'Becky Shaw', 'Max Garrett'],
+    [s70, s70_bt, actor, "The Winter's Tale", "Shepherd's Son"],
+    [s70, s70_bt, lights, "The Winter's Tale"],
+    [s70, s70_bt, actor, 'Much Ado About Nothing', 'Conrad'],
+    [s70, s70_bt, actor, 'As You Like It', 'Silvius'],
+    [s70, s70_bt, actor, "A Midsummer Night's Dream", 'Peter Quince'],
+    [s70, s70_bt, actor, 'Twelfth Night', 'Sir Andrew Aguecheek'],
+    [s70, s70_bt, actor, 'The Tempest', 'Sebastian'],
+    [s70, s70_bt, actor, 'The Importance of Being Earnest', 'Gribsby'],
+    [s70, s70_bt, actor, 'A View from the Bridge', 'Mike/Immigration Officer'],
+    [s70, s70_bt, sm, 'A View from the Bridge'],
+    [s70, s70_bt, actor, 'Romeo and Juliet', "Sampson/Apothecary/Paris's Page"],
+    [s70, s70_bt, actor, 'The Skin of Our Teeth', 'Mr. Fitzpatrick'],
+    [s70, s70_bt, sm, 'The Skin of Our Teeth']
+
+].each do |casting|
+  Casting.where(member: mike,
+                production: Production.where(company: casting[0], title: casting[3]).first_or_create!,
+                position: casting[2],
+                bio_type: casting[1]).first_or_create!(role: casting[4])
+end
+
+[
+  ['Playmasters Playhouse', oc_bt, actor, '6 Rms Riv Vu', 'Paul Friedman'],
+  ['Playmasters Playhouse', oc_bt, actor, 'Barefoot in the Park', 'Paul Bratter'],
+  ['ActorsNET of Bucks County', oc_bt, actor, 'Sherlock Holmes', 'Sherlock Holmes'],
+  ['ActorsNET of Bucks County', oc_bt, actor, 'The Man Who Came to Dinner', 'Beverly Carlton/Sandy'],
+  ['ActorsNET of Bucks County', oc_bt, actor, 'Uncle Vanya', 'Yefim'],
+  ['ActorsNET of Bucks County', oc_bt, sm, 'Uncle Vanya'],
+  ['ActorsNET of Bucks County', oc_bt, actor, 'Much Ado About Nothing', 'Verges'],
+  ['ActorsNET of Bucks County', oc_bt, sm, 'Much Ado About Nothing'],
+  ['Langhorne Players', oc_bt, actor, "It's Only a Play", 'Frank Finger']
+].each do |casting|
+  Casting.where(member: mike,
+                production: Production.where(company: Company.where(name: casting[0]).first_or_create!,
+                                             title: casting[3]).first_or_create!,
+                position: casting[2],
+                bio_type: casting[1]).first_or_create!(role: casting[4])
+end
