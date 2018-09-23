5a832df4294eff69a5ff6bb6aa8e816109aa2577
Raymond Fallon <Raymond.Fallon@pennmedicine.upenn.edu>
2018-09-22 20:34:20 -0400

    Change Header Hover Behavior

--- db/seeds/members/ray_fallon.rb
+++ db/seeds/members/ray_fallon.rb
@@ -7,4 +7,37 @@ ray = Member.where(first_name: 'Ray', last_name: 'Fallon')
                               featured: 'Y',
                               title: 'Actor • Webmaster',
                               bio: "RAY HASN'T WRITTEN A BIO! But that's OK.")
+
 s70 = Company.where(name: "Shakespeare '70").first_or_create!
+
+s70_bt = BioType.where(code: 'S70', description: 'Shakespeare 70').first_or_create!
+oc_bt =  BioType.where(code: 'OC', description: 'Other Company').first_or_create!
+
+actor =    Position.where(code: 'A', description: 'Actor').first_or_create!
+sm =       Position.where(code: 'SM', description: 'Stage Manager').first_or_create!
+asm =      Position.where(code: 'ASM', description: 'Assistant Stage Manager').first_or_create!
+pm =       Position.where(code: 'PM', description: 'Production Manager').first_or_create!
+
+[
+  [s70_bt, actor, 'Murder Most Foul: An Evening of Shakespeare', 'Ensemble'],
+  [s70_bt, pm, 'Murder Most Foul: An Evening of Shakespeare'],
+  [s70_bt, actor, 'Hamlet', 'Hamlet'],
+  [s70_bt, actor, "The Winter's Tale", 'Polixenes'],
+  [s70_bt, actor, 'Becky Shaw', 'Andrew Porter'],
+  [s70_bt, actor, 'The Vortex', 'Nicky'],
+  [s70_bt, actor, 'Measure For Measure', 'Angelo'],
+  [s70_bt, sm, 'The Heidi Chronicles'],
+  [s70_bt, actor, 'Hedda Gabler', 'Eilert Løvborg'],
+  [s70_bt, actor, 'Much Ado About Nothing', 'Benedick'],
+  [s70_bt, actor, 'As You Like It', 'Orlando'],
+  [s70_bt, actor, 'Twelfth Night', 'Sea Captain'],
+  [s70_bt, actor, 'The Tempest', 'Ferdinand'],
+  [s70_bt, actor, 'Romeo and Juliet', 'Paris'],
+  [s70_bt, actor, 'The Importance of Being Earnest', 'Algernon'],
+  [s70_bt, asm, 'Candida']
+].each do |casting|
+  Casting.where(member: ray,
+                production: Production.where(company: s70, title: casting[2]).first_or_create!,
+                position: casting[1],
+                bio_type: casting[0]).first_or_create!(role: casting[3])
+end
