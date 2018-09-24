35e4590f975407e6bfe21ac944e2e2f0a70ae7a8
Raymond Fallon <Raymond.Fallon@pennmedicine.upenn.edu>
2018-09-24 16:09:43 -0400

    Add Custom 404 Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#show'
 
+  # GET route names from old Wordpress site
+  get :previous_productions, to: 'productions#index'
+
   get :black_box_directions, to: 'static_pages#black_box_directions', as: :black_box_directions
   get :community, to: 'static_pages#community', as: :community
   get :donations, to: 'static_pages#donations', as: :donations
---
+++ db/seeds/productions/circle_mirror.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+s70 = Company.find_by(name: "Shakespeare '70")
+
+wwac = Venue.find_by(name: 'West Windsor Arts Center')
+
+s70_bt = BioType.find_by(code: 'S70')
+
+cmt = Production.where(company: s70,
+                          venue: wwac,
+                          title: 'Circle Mirror Transformation',
+                          year: 2014).first_or_create!
+
+[ # Cast
+  ['Schultz', 'Ernie', 'Albanesius'],
+  ['James', 'Scott', 'Karlin'],
+  ['Lauren', 'Tara', 'Mershon'],
+  ['Theresa', 'Maryalice', 'Rubins-Topoleski'],
+  ['Marty', 'Carol', 'Thompson']
+].each_with_index do |casting, idx|
+  Casting.where(member: Member.where(first_name: casting[1], last_name: casting[2]).first_or_create!,
+                production: cmt,
+                position: Position.find_by(code: 'A'),
+                bio_type: s70_bt).first_or_create!.update(role: casting[0], cast_order: idx)
+end
+
+
+
+[
+  ['Director', 'Janet', 'Quartarone'],
+  ['Assistant Director', 'Lili', 'Timmes'],
+  ['Stage Manager', 'Andrew', 'Timmes'],
+  ['Lighting Designer', 'Michael', 'Krahel'],
+  ['Business Manager', 'Tom', 'Curbishley']
+].each do |casting|
+  Casting.where(member: Member.where(first_name: casting[1], last_name: casting[2]).first_or_create!,
+                production: cmt,
+                position: Position.find_by(description: casting[0]),
+                bio_type: s70_bt).first_or_create!
+end
+
+cmt.update(has_all_data: 'Y')
+
