c252d873caf60a6139dd08717d4b772afaff1051
Raymond Fallon <Raymond.Fallon@pennmedicine.upenn.edu>
2018-10-14 18:16:46 -0400

    Update Tom Bio

---
+++ db/seeds/members/tom_stackhouse.rb
@@ -0,0 +1,4 @@
+
+Member.where(first_name: 'Tom', last_name: 'Stackhouse')
+      .update(title: 'Actor',
+              bio: "Tom acts in the Philadelphia area doing short films, commercials, and videos. He works as a Standardized Patient at a Philadelphia medical school. Tom’s recent musical projects include playing with the Ong’s Hat Band and self-producing <em>Tragedy. Mystery. Hope.</em>  A solo CD of original songs about the history and lore of the NJ Pine Barrens. He sings, plays guitar and mandolin on the album.")
